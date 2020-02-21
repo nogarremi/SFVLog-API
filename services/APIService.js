e3e38db30c6ce8e22e55e4cbba1253985afe5868
nogarremi <nogarremi@nogarremi.com>
2020-02-20 21:37:09 -0800

    POST works
    Values caught if input too fast

--- services/APIService.js
+++ services/APIService.js
@@ -137,7 +137,7 @@ exports.getMatches = function(req, res) {
     })
 };
 
-/*exports.postMatch = function(req, res) {
+exports.postMatch = function(req, res) {
     let season = req.body.season;
     let type = req.body.type;
     let result = req.body.result;
@@ -145,6 +145,7 @@ exports.getMatches = function(req, res) {
     let opp_char = req.body.opp_char;
     let opp_name = req.body.opp_name;
     let opp_rank = req.body.opp_rank;
+    var sql = '(SELECT opp_id FROM opponents WHERE opp_name = "' + opp_name + '")';
     
     opponents.findAll({
         attributes: ["opp_id", "opp_rank_id", "opp_name"]
@@ -155,21 +156,11 @@ exports.getMatches = function(req, res) {
             names.push(item.opp_name);
         })
         if (names.includes(opp_name)){
-            opponents.update({opp_rank_id: opp_rank}, {where: {opp_name: opp_name}});
+            opponents.update({opp_rank_id: opp_rank}, {where: {opp_name: opp_name}}).then(matches.create({season: season, match_type: type, my_char_id: my_char, opp_id: seq.literal(sql), opp_char_id: opp_char, result: result}));
         }
         else {
-            opponents.create({ opp_name: opp_name, opp_rank_id: opp_rank});
+            opponents.create({ opp_name: opp_name, opp_rank_id: opp_rank}).then(matches.create({season: season, match_type: type, my_char_id: my_char, opp_id: seq.literal(sql), opp_char_id: opp_char, result: result}));
         }
-        var sql = '(SELECT opp_id FROM opponents WHERE opp_name = ' + opp_name + ')';
-        var new_match = await matches.create({season: season, match_type: type, my_char_id: my_char, opp_id: seq.literal(sql), opp_char_id: opp_char, result: result});
-        if (!new_match){
-            return res.status(400).send({
-                message: "No matches found!"
-            })
-        }
-        return res.status(200).send({
-            results: new_match
-        })
+        return res.status(200).send([season, type, result, my_char, opp_char, opp_name, opp_rank]);
     })
 };
-*/
